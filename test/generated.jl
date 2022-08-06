166e6f6b80e635dac894b1b96878664d9cd0b34d
thautwarm <twshere@outlook.com>
2022-08-06 17:08:50 +0800

    up

--- test/generated.jl
+++ test/generated.jl
@@ -339,7 +339,7 @@ function lex(construct_token,  lexerbuf::lexbuf)
     return construct_token(token_id, sedlex_lexeme(lexerbuf), lexerbuf.start_line, lexerbuf.pos - lexerbuf.curr_bol, lexerbuf.pos - lexerbuf.start_pos, lexerbuf.start_pos, lexerbuf.filename)
 end
 function lexall(construct_token, buf::lexbuf, is_eof #= Token -> Bool =#)
-    Channel{Token}(0) do coro
+    (@static VERSION < v"1.3" ? Channel : Channel{Token})() do coro
         while true
             token = lex(construct_token, buf)
             token === nothing && continue
